d94dd1160db2b1be91742bc55e866cbbba81a60b
sivanesan1103 <93029538+sivanesan1103@users.noreply.github.com>
2025-10-08 00:57:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -59,17 +59,7 @@ app.get("/", function (req, res) {
 //bg
 function bg() {
   let images = [
-    "https://media.giphy.com/media/cJGRxpn526t54iyBCd/giphy.gif",
-    "https://media.giphy.com/media/KdvsBU5PTlNDXV58Ft/giphy.gif",
-    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZWJjYnJ5aGsxMTltbmJzcWh2ZHc4M3I3em05MWlranhudTl1MjQ1YSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/TElxFaP5cJBZCid3gG/giphy.gif",
-    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdjY5a3FpdGYwamdweXhmanoyYXF0dG1iN2libnQ4emhuZnduMGdqMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/alDD6QqHMWUQU/giphy.gif",
-    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGoyZm1iaGYyc3BxMHpydmIydm8yNzhqcHJtZG45czZoNmpvaHg3dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l03KzXLrzlRGkwLkhA/giphy.gif",
-    "https://media.giphy.com/media/26zzaI4GRWtcVGDi8/giphy.gif",
-    "https://media.giphy.com/media/rAKdqZ8nfiaZi/giphy.gif",
-    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2pvY3A4aHNsNWI2Z2hlbXozYjdqcm1xNHNxcnR4NzNjeWZjdHRmYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/OPcbISBoXhh5u/giphy-downsized-large.gif",
-    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaDVzcmZ3Y3lrdzd6MDUzcDlkeWl1aGlqeWozeTZwazd4bjB4anJldCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/faIED06xwJmnu/giphy.gif",
-    "https://s2.pictoa.com/media/galleries/272/275/2722755fd7c4def30df/36868665fd7c4e56cc0d.gif",
-    "https://media.giphy.com/media/3o6Yg2TN9E59G9vSxi/giphy.gif",
+
   ];
 
   const bg = images[Math.floor(Math.random() * images.length)];
@@ -239,3 +229,4 @@ app.get("/posts/:topic", (req, res) => {
 app.listen(port, function (req, res) {
   console.log("server is up");
 });
+
